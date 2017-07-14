c2a7aaf3f65e0103de90a58fbfff1b202672486a
david <davidleitner@live.at>
2017-07-14 22:04:10 +0200

    Adding Javascript Boilerplate

---
+++ javascript/lib/NotImplementedError.js
@@ -0,0 +1,9 @@
+class NotImplementedError extends Error{
+	constructor(){
+		super();
+		this.name = "NotImplementedError";
+		this.stack = (new Error()).stack;
+	}
+}
+
+module.exports = NotImplementedError;
