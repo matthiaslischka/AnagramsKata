5bfeb295a4c7bfb481772ab0783cb6679c70617f
fflasch <ffl@mission-embedded.com>
2017-08-21 14:52:20 +0200

    Setup Unity testframework

---
+++ c/anagrams_kata/tests.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "unity.h"
+
+void test_Anagrams_kata(void)
+{
+    TEST_FAIL_MESSAGE("Not implemented");
+}
+
+void setUp(void)
+{
+    //printf("setUp\n");
+}
+
+void tearDown(void)
+{
+    //printf("tearDown\n");
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_Anagrams_kata);
+    return UNITY_END();
+}
